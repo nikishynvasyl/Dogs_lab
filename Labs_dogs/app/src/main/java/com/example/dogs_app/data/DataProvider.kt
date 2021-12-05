62186676bd549e57c60c5aa8e1160880818110b2
nikishynvasyl <73739856+nikishynvasyl@users.noreply.github.com>
2021-12-05 17:37:37 +0200

    final

---
+++ Labs_dogs/app/src/main/java/com/example/dogs_app/data/DataProvider.kt
@@ -0,0 +1,105 @@
+package com.example.dogs_app.data
+
+import com.example.dogs_app.R
+
+object DataProvider {
+
+    val puppyList = listOf(
+        Puppy(
+            id = 1,
+            title = "Akita",
+            sex = "Male",
+            age = 6,
+            description = "These dogs have a calm nature, which is why they are often adopted in families with children.",
+            puppyImageId = R.drawable.p1
+        ),
+        Puppy(
+            id = 2,
+            title = "Baste",
+            sex = "Female",
+            age = 10,
+            description = "Baste is Alabai. These dogs are strong and endurance. By nature, they are leaders and defenders. They usually live well with children and other animals, but may behave inappropriately with strangers.",
+            puppyImageId = R.drawable.p2
+        ),
+        Puppy(
+            id = 3,
+            title = "Richi",
+            sex = "Male",
+            age = 9,
+            description = "Richi is an Alaskan Malamute. Such animals are very kind and accommodating, very easy to train, but, despite this, it must be professional.",
+            puppyImageId = R.drawable.p3
+        ),
+        Puppy(
+            id = 4,
+            title = "Baby",
+            sex = "Male",
+            age = 13,
+            description = "Baby is American Staffordshire Terrier. Dogs of this breed are excellent defenders and guards, but only if they are professionally trained.",
+            puppyImageId = R.drawable.p4
+        ),
+        Puppy(
+            id = 5,
+            title = "Basenji",
+            sex = "Female",
+            age = 11,
+            description = "Dogs of this breed are intelligent and clever, love games and attention to their person.",
+            puppyImageId = R.drawable.p5
+        ),
+        Puppy(
+            id = 6,
+            title = "Pitty",
+            sex = "Female",
+            age = 5,
+            description = "Pitty American Pit Bull Terrier. Pupils of this breed have not found a very good reputation, however, in fact they are very loyal and friendly. The whole problem lies in the proper and timely education of pit bulls.",
+            puppyImageId = R.drawable.p6
+        ),
+        Puppy(
+            id = 7,
+            title = "Basset",
+            sex = "Male",
+            age = 10,
+            description = "These dogs are quite obedient, but are extremely difficult to train. The breed itself is very slow, but at the same time inquisitive.",
+            puppyImageId = R.drawable.p7
+        ),
+        Puppy(
+            id = 8,
+            title = "Bloodhound",
+            sex = "Female",
+            age = 9,
+            description = "Dogs of this breed are extremely friendly, love children and often act as their nanny.",
+            puppyImageId = R.drawable.p8
+        ),
+        Puppy(
+            id = 9,
+            title = "Bully",
+            sex = "Male",
+            age = 7,
+            description = "By nature, these dogs are intelligent and loyal hooligans. An untrained boxer is sure to bring you a lot of trouble.",
+            puppyImageId = R.drawable.p9
+        ),
+        Puppy(
+            id = 10,
+            title = "Bobtail",
+            sex = "Female",
+            age = 11,
+            description = "Dogs of this breed need constant exercise because they are very active and mobile.",
+            puppyImageId = R.drawable.p10
+        ),
+        Puppy(
+            id = 11,
+            title = "Beauceron",
+            sex = "Male",
+            age = 14,
+            description = "Dogs of this breed are born leaders, have guarding and protective qualities.",
+            puppyImageId = R.drawable.p11
+        ),
+        Puppy(
+            id = 12,
+            title = "Chappy",
+            sex = "Female",
+            age = 15,
+            description = "Chappy is Bull Terrier. The fame of this breed, of course, is not good. But, despite this, bull terriers are good dogs that need early training and socialization.",
+            puppyImageId = R.drawable.p12
+        ),
+    )
+}
